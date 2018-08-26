33b0e26364a1d9848de3d5ccc8dbc2122ece28bb
GTGamer <33270709+GTGamer@users.noreply.github.com>
2018-08-26 19:07:03 -0400

    Attack Implementation

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -24,6 +24,7 @@ public class PlayerController : MonoBehaviour {
     public float dashCooldown = 0.6f;
 	public float dashCooldown2 = 0.3f;
 	public float dashLength = 0.05f;
+	public float attackCooldown = 0.6f;
 	public float speed = 20f;
 	// public Sprite[] runningForwardSheet;
 	// public Sprite[] runningForwardRightStance;
@@ -34,11 +35,11 @@ public class PlayerController : MonoBehaviour {
 	public Sprite[] idle;
 	//public Sprite[] attackF;
 	public Sprite[] stances;
-	public Sprite[] dashing;
 	public int direction;
 	private int running = 0;
 	//private int attack = 0;
 	private int idleC = 0;
+	private int attackC = 0;
 	private int stance = 0;
 	private SpriteRenderer spriteR;
     [Serializable]
@@ -48,9 +49,10 @@ public class PlayerController : MonoBehaviour {
         public Sprite image;
     }
     public NamedImage[] pictures;
-    private Sprite[,,,] runHash = new Sprite[2, 4, 3, 3];
 	private enum Direction {Forward, Backward};
 	private enum Stance {Left, High, Right, Low};
+	private enum Action {Idle, Run, Attack, Dash};
+    private Sprite[,,,] runHash = new Sprite[2, 4, 4, 3];
     private void loadHash()
     {
 		/*
@@ -75,18 +77,17 @@ public class PlayerController : MonoBehaviour {
             runHash[2].Add(pictures[loadingStance].name, pictures[loadingStance].image);
         }*/
 
-		for (int x = 0; x < 18; x++)
+		for (int x = 0; x < 3; x++)
 		{
-			int dir = (x%6)/3;
-			int stance = x/6;
-			int frame = x%3;
-			
-			runHash[dir, stance, 1, frame] = pictures[x].image;
-		}
-
-		for(int x = 0; x < 4; x++){
-			runHash[0, x, 2, 0] = dashing[0];
-			runHash[1, x, 2, 0] = dashing[1];
+			for(int y = 0; y < 6; y++) {
+				runHash[y/3, x, Action.Run.GetHashCode(), y%3] = pictures[x*11+y].image;
+			}
+			for(int y = 0; y < 3; y++) {
+				runHash[Direction.Forward.GetHashCode(), x, Action.Attack.GetHashCode(), y] = pictures[x*11+y+6].image;
+				runHash[Direction.Backward.GetHashCode(), x, Action.Attack.GetHashCode(), y] = pictures[x*11+y+6].image;
+			}
+			runHash[Direction.Forward.GetHashCode(), x, Action.Dash.GetHashCode(), 0] = pictures[x*11+9].image;
+			runHash[Direction.Backward.GetHashCode(), x, Action.Dash.GetHashCode(), 0] = pictures[x*11+10].image;
 		}
 
     }
@@ -94,8 +95,10 @@ public class PlayerController : MonoBehaviour {
 
 	private bool isMoving;
 	private bool isDashing;
+	private bool isAttacking;
 
-	private float timer;
+	private float dashtimer;
+	private float attacktimer;
     // Use this for initialization
     void Start () {
 		anim = GetComponent<Animator> ();
@@ -110,12 +113,15 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetKey(KeyCode.Semicolon)) {
 			isDashing = true;
 		}
+		if (Input.GetKey (KeyCode.U)) {
+			isAttacking = true;
+		}
 		if (Input.GetKey(KeyCode.J)) {
 			stance = Stance.Left.GetHashCode();
 		}
-		if (Input.GetKey(KeyCode.K)) {
-			stance = Stance.Low.GetHashCode();
-		}
+		// if (Input.GetKey(KeyCode.K)) {
+		// 	stance = Stance.Low.GetHashCode();
+		// }
 		if (Input.GetKey(KeyCode.I)) {
 			stance = Stance.High.GetHashCode();
 		}
@@ -128,33 +134,49 @@ public class PlayerController : MonoBehaviour {
 		} else if (Input.GetKey (KeyCode.A)) {
 			isMoving = true;
 			direction = Direction.Backward.GetHashCode();
-		}  
+		}
 		else {
 			isMoving = false;
+			isDashing = false;
 		}
 	}
 
 	//runs at a fixed rate.
 	void FixedUpdate()
 	{
-		if (isMoving) {
+		if(attacktimer > 0) {
+			attacktimer += Time.deltaTime;
+			if (attacktimer >= attackCooldown) {
+				attacktimer = 0;
+			}
+		}
+		if (isAttacking && attacktimer == 0) {
+			spriteR.sprite = runHash[direction, stance, Action.Attack.GetHashCode(), attackC/5];
+			attackC++;
+			if (attackC == 15) {
+				attackC = 0;
+				isAttacking = false;
+				attacktimer = 0.001f;
+			}
+		}
+		else if (isMoving) {
 			if (isDashing) {
-				timer += Time.deltaTime;
+				dashtimer += Time.deltaTime;
 
-				if (timer <= dashLength) { 	//while within the threshold, animation will run.
-					spriteR.sprite = runHash[direction, stance, 2, 0];
+				if (dashtimer <= dashLength) { 	//while within the threshold, animation will run.
+					spriteR.sprite = runHash[direction, stance, Action.Dash.GetHashCode(), 0];
 					transform.Translate ((direction * (-2) + 1) * Time.deltaTime * speed * 5f, 0, 0);
-				} else if (timer >= dashLength + dashCooldown2) {
+				} else if (dashtimer >= dashLength + dashCooldown2) {
 					Running ();
 					transform.Translate ((direction * (-2) + 1) * Time.deltaTime * speed, 0, 0);
 					//SetAnims ("isMovingRight", true);
 				} else {
-					spriteR.sprite = runHash[direction, stance, 2, 0];
+					spriteR.sprite = runHash[direction, stance, Action.Dash.GetHashCode(), 0];
 					transform.Translate ((direction * (-2) + 1) * Time.deltaTime * speed, 0, 0);
 				}
-				if (timer >= dashLength + dashCooldown) {
+				if (dashtimer >= dashLength + dashCooldown) {
 					isDashing = false;
-					timer = 0;
+					dashtimer = 0;
 				}
 			} else {
 				Running ();
@@ -163,9 +185,11 @@ public class PlayerController : MonoBehaviour {
 			}
 		} else {
 			//SetAnims ("isMovingRight", false);
-			if(isDashing) {
-				isDashing = false;
-				timer = 0;
+			if(dashtimer > 0) {
+				dashtimer += Time.deltaTime;
+				if (dashtimer >= dashLength + dashCooldown) {
+					dashtimer = 0;
+				}
 			}
 			Idle();
 		}
@@ -176,7 +200,7 @@ public class PlayerController : MonoBehaviour {
 		if (running == 15) {
 			running = 0;
 		}
-		spriteR.sprite = runHash[direction, stance, 1, running / 5];
+		spriteR.sprite = runHash[direction, stance, Action.Run.GetHashCode(), running / 5];
 		// if (direction == 1 && running % 5 == 0) {
         //     spriteR.sprite = (Sprite)runHash[stance]["Forwards"];
 
